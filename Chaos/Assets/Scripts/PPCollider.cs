e1fb10943a0bb326df29c54bc8db398ca5aea73e
Guy Balaam <guy.balaam@gmail.com>
2021-08-24 23:02:03 +0100

    Some PP Collision Thingies

---
+++ Chaos/Assets/Scripts/PPCollider.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class PPCollider : MonoBehaviour
+{
+    private SpriteRenderer renderer;
+
+    // Unity connected
+    public Sprite foreground;
+
+    public void Start()
+    {
+        renderer = GetComponent<SpriteRenderer>();
+        renderer.sprite = foreground;
+    }
+
+    private void Update()
+    {
+        Debug.Log(CircleCollision(World2Pixel(Vector3.zero), 8));
+        // Debug.Log(PointCollision(World2Pixel(Vector3.zero)));
+    }
+
+    public Vector2Int World2Pixel(Vector3 worldPos)
+    {
+        float ppu = foreground.pixelsPerUnit;
+
+        var worldOffset = (worldPos - transform.position) * ppu;
+        var spriteOffset = new Vector2Int(foreground.texture.width / 2, foreground.texture.height / 2);
+
+        return new Vector2Int((int)(worldOffset.x), (int)(worldOffset.y)) + spriteOffset;
+    }
+
+    public bool PointCollision(Vector2Int pixel)
+    {
+        Color c = foreground.texture.GetPixel(pixel.x, pixel.y);
+        return c.a != 0; // Not transparent
+    }
+
+    private float Hypot(int a, int b)
+    {
+        return Mathf.Sqrt(Mathf.Pow(a, 2) + Mathf.Pow(b, 2));
+    }
+
+    public bool CircleCollision(Vector2Int center, int radius)
+    {
+        for (int height = radius; height >= -radius; height--)
+        {
+            int y = center.y - height;
+
+            if (PointCollision(new Vector2Int(center.x, y))) return true;
+
+
+            for (int width = 1; Hypot(height, width) < radius + 0.5f; width++)
+            {
+                int x1 = center.x + width;
+                int x2 = center.x - width;
+                if (PointCollision(new Vector2Int(x1, y))) return true;
+                if (PointCollision(new Vector2Int(x2, y))) return true;
+            }
+        }
+        return false;
+    }
+
+    private void SetTexture(SpriteRenderer r, Texture2D t)
+    {
+        // Let's see if we can optimise this later and avoid uneccessary instantiations...
+        r.sprite = Sprite.Create(t, new Rect(0, 0, t.width, t.height), Vector2.one * 0.5f);
+    }
+}
